0cc8276e7a0cd24b6d29b1663925445600104885
Ринат Ахметов <appvshare@gmail.com>
2015-11-07 16:00:05 +0300

    make filter on picture blur boxFilter gaussianBlur medianBlur and add haar cascade for eye

---
+++ source/7.py
@@ -0,0 +1,27 @@
+__author__ = 'tz3'
+# make filter on picture
+# blur
+# boxFilter
+# gaussianBlur
+# medianBlur
+
+import cv2
+
+import config
+
+
+def filters():
+    image = cv2.imread(config.ORIGIN_IMAGE)
+    blur = [cv2.GaussianBlur(image, (5, 5), 0)]
+    blur.append(cv2.medianBlur(image, 5))
+    blur.append(cv2.boxFilter(image, 5, (5, 5)))
+    for i, x in enumerate(blur):
+        cv2.imwrite(
+            config.RESULT_PATH + "%s_mode_%s.jpg" % (
+                config.current_filename(__file__), i),
+            x)
+
+
+if __name__ == "__main__":
+    # video doesn't play
+    filters()
